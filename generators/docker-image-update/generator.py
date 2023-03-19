8533f3210ee7f60716e2d6696ab4e86806643604
Noah <burghardtnoah@gmail.com>
2023-03-18 18:47:51 -0600

    minor cleanup

--- generators/docker-image-update/generator.py
+++ generators/docker-image-update/generator.py
@@ -2,12 +2,16 @@ import sys, json
 data = json.loads(sys.stdin.read())
 template = """
 $HOST:$CONTAINER:
+  stage: deploy
   when: manual
   script:
     - echo "I am deploying to $HOST -> $CONTAINER"
 """
 
-output = ""
+output = """
+stages:
+  - deploy
+"""
 for host in data:
     for ctr in data[host]:
         output += template.replace("$HOST", host).replace("$CONTAINER", ctr)
