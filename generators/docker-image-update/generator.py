5b832f0865f25f5a660d5e170df4913e1849f4bc
Noah <burghardtnoah@gmail.com>
2023-03-18 18:35:01 -0600

    updated docker image update py script

---
+++ generators/docker-image-update/generator.py
@@ -0,0 +1,15 @@
+import sys, json
+data = json.loads(sys.stdin.read())
+template = """
+$HOST:$CONTAINER:
+  when: manual
+  script:
+    - echo "I am deploying to $HOST -> $CONTAINER"
+"""
+
+output = ""
+for host in data:
+    for ctr in data[host]:
+        output += template.replace("$HOST", host).replace("$CONTAINER", ctr)
+
+print(output)
